31db4f326da3f3dd7ef906a20952ef3b4c23b9c2
amandavinci <aman@amandavinci.me>
2017-10-20 13:08:36 +0530

    Modify setup scripts to download appropriate cuDNN libraries for cuda 8.0

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,12 @@
+echo"
 #####################################
 #   Setup a Deep Learning Machine   #
 #   	[Ubuntu 16.04 LTS]          #
 #####################################
+"
 
 # ensure system is updated and has basic build tools
-echo "System Update & Upgrade"
+echo "\n\nSystem Update & Upgrade\n\n"
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
 sudo apt-get --assume-yes install tmux build-essential gcc g++ make binutils
@@ -13,7 +15,7 @@ sudo apt-get --assume-yes install software-properties-common
 # download and install GPU drivers
 # see https://cloud.google.com/compute/docs/gpus/add-gpus#install-gpu-driver
 
-echo "Checking for CUDA & Installing..."
+echo "\n\nChecking for CUDA & Installing...\n\n"
 # Check for CUDA and try to install.
 if ! dpkg-query -W cuda; then
   curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
@@ -22,23 +24,23 @@ if ! dpkg-query -W cuda; then
   sudo apt-get install cuda -y
 fi
 
-echo "Verifying GPU driver installation"
+echo "\n\nVerifying GPU driver installation\n\n"
 # verify that GPU driver installed
 sudo modprobe nvidia
 nvidia-smi
 
 sudo apt-get install libcupti-dev
 
-echo "Downloading Anaconda..."
+echo "\n\nDownloading Anaconda...\n\n"
 # install Anaconda for current user
 mkdir downloads
 cd downloads
 wget "https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh" -O "Anaconda3-4.3.1-Linux-x86_64.sh"
 
-echo "Installing Anaconda..."
+echo "\n\nInstalling Anaconda...\n\n"
 bash "Anaconda3-4.3.1-Linux-x86_64.sh" -b
 
-echo "Setting up the Anaconda path"
+echo "\n\nSetting up the Anaconda path\n\n"
 echo "export PATH=\"$HOME/anaconda3/bin:\$PATH\"" >> ~/.bashrc
 export PATH="$HOME/anaconda3/bin:$PATH"
 conda install -y bcolz
@@ -46,7 +48,7 @@ conda upgrade -y --all
 
 
 # install and configure theano
-echo "Installing & Configuring Theano..."
+echo "\n\nInstalling & Configuring Theano...\n\n"
 conda install theano pygpu
 echo "[global]
 device = cuda0
@@ -55,7 +57,7 @@ floatX = float32
 root = /usr/local/cuda" > ~/.theanorc
 
 # install and configure keras
-echo "Installing & Configuring Keras..."
+echo "\n\nInstalling & Configuring Keras...\n\n"
 conda install keras
 mkdir ~/.keras
 echo '{
@@ -66,19 +68,22 @@ echo '{
 }' > ~/.keras/keras.json
 
 # install cudnn libraries
-echo "Downloading cuDNN libraries from Google Drive..."
-chmod +x gdown.pl
-./gdown.pl "https://drive.google.com/uc?export=download&confirm=OlUi&id=0B1KAQ69tr5ovNi12Qk5DUEV6NDg" "cudnn.tgz"
-# wget "http://files.fast.ai/files/cudnn.tgz" -O "cudnn.tgz"
 
-echo "Installing cuDNN..."
+# echo "Downloading cuDNN libraries from Google Drive..."
+# chmod +x gdown.pl
+# ./gdown.pl "https://drive.google.com/uc?export=download&confirm=OlUi&id=0B1KAQ69tr5ovNi12Qk5DUEV6NDg" "cudnn.tgz"
+
+echo "\n\nDownloading cuDNN libraries from fast.ai archive...\n\n"
+wget "http://files.fast.ai/files/cudnn.tgz" -O "cudnn.tgz"
+
+echo "\n\nInstalling cuDNN...\n\n"
 tar -zxf cudnn.tgz
 cd cuda
 sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
 
 # configure jupyter and prompt for password
-echo "Configuring jupyter and will prompt for password..."
+echo "\n\nConfiguring jupyter and will prompt for password...\n\n"
 jupyter notebook --generate-config
 jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
 echo "c.NotebookApp.ip = '*'

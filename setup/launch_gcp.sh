102eb0e9c4fbb7b0309284a381a8138738f9a6ad
amandavinci <amanthevinci@gmail.com>
2019-02-06 11:25:53 +0530

    add script to create gcp vm

--- setup/launch_gcp.sh
+++ setup/launch_gcp.sh
@@ -1,7 +1,7 @@
 export IMAGE_FAMILY="pytorch-latest-gpu" # or "pytorch-latest-cpu" for non-GPU instances
-export ZONE="asia-southeast1-b" # budget: "us-west1-b"
-export INSTANCE_NAME="aman"
-export INSTANCE_TYPE="n1-highmem-8" # budget: "n1-highmem-4"
+export ZONE="asia-east1-b" # budget: "us-west1-b"
+export INSTANCE_NAME="davinci"
+export INSTANCE_TYPE="n1-standard-8" # budget: "n1-highmem-4"
 
 # budget: 'type=nvidia-tesla-k80,count=1'
 gcloud compute instances create $INSTANCE_NAME \
@@ -9,7 +9,7 @@ gcloud compute instances create $INSTANCE_NAME \
         --image-family=$IMAGE_FAMILY \
         --image-project=deeplearning-platform-release \
         --maintenance-policy=TERMINATE \
-        --accelerator="type=nvidia-tesla-p4,count=1" \
+        --accelerator="type=nvidia-tesla-k80,count=1" \
         --machine-type=$INSTANCE_TYPE \
         --boot-disk-size=200GB \
         --metadata="install-nvidia-driver=True" \
